81a7cf4be5c6ed152ebb0eaa1ddb8f01c3daf641
mielecarmine <mielecarmine993@gmail.com>
2024-04-17 16:57:00 +0200

    add: AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -29,7 +29,36 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="container mt-5">
+    <label for="characters"><b class="fs-4">Choose a character:</b></label>
+    <select class="form-select" name="characters" id="characters">
+      <option v-for="charact in store.characters" :value="charact.id">
+        {{ charact.name }}
+      </option>
+      <!-- <option value="2">Elena</option>
+      <option value="3">Gareth</option>
+      <option value="4">Sylvia</option>
+      <option value="5">Liam</option>
+      <option value="6">Isabella</option>
+      <option value="7">Oscar</option>
+      <option value="8">Aria</option>
+      <option value="9">Victor</option>
+      <option value="10">Luna</option>
+      <option value="11">Xander</option>
+      <option value="12">Sophia</option>
+      <option value="13">Maximus</option> -->
+    </select>
+    <div class="row mt-5">
+      <div class="col">
+        <char-card />
+      </div>
+      <div class="col">
+        <char-card />
+      </div>
+    </div>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 @use "../style/partials/mixins" as *;
