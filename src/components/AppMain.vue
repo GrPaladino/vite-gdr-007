00ab666bb4e9389273d1819fed2594836c397dd0
GrPaladino <paladino.graziano@gmail.com>
2024-04-17 16:32:29 +0200

    Feat: fetchApi created

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,26 +1,33 @@
 <script>
 // import { store } from "../store";
+import axios from "axios";
 
 export default {
   data() {
     return {
-      // store,
-    }
+      characters: [],
+    };
   },
 
-  components: {
-  },
+  components: {},
 
   methods: {
+    fetchCharacters() {
+      axios.get("http://127.0.0.1:8000/api/characters").then((response) => {
+        this.characters = response.data.characters;
+      });
+    },
+  },
+
+  created() {
+    this.fetchCharacters();
   },
 };
 </script>
 
-<template>
-</template>
+<template></template>
 
 <style lang="scss" scoped>
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
-
 </style>
